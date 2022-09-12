190f3ee8ffc4db0abc394dc49d8a4d952b46745b
Taj Wali Khan <taj.physicist@gmail.com>
2022-09-12 19:07:13 +0800

    first commit

---
+++ pract.js
@@ -0,0 +1,33 @@
+let string = '';
+
+let btns = document.querySelectorAll('.btn');
+
+Array.from(btns).forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.querySelector('input').value= string;
+        }
+        else if (e.target.innerHTML=='AC'){
+            string='';
+            document.querySelector('input').value= string;
+        }
+        else if( e.target.innerHTML=='+/-'){
+            string=-string;
+            document.querySelector('input').value= string;
+        }
+        else if (e.target.innerHTML=='%'){
+            string=string/100;
+            document.querySelector('input').value= string;
+        }
+        // else if (e.target.innerHTML=='x'){
+        //     x==*
+        //     document.querySelector('input').value= string;
+        // }
+        else{
+            console.log(e.target);
+            string = string+ e.target.innerHTML;
+            document.querySelector('input').value= string;
+        }
+    })
+})
